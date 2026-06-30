aa6d07fdc5c7f0d660dc106b2bee4cb382b37d8c
Aproteem Majumder <axm220330@utdallas.edu>
2026-06-30 15:17:18 -0500

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            l, r = i+1, len(nums)-1
+
+            if(nums[i]>0):
+                #in sorted if my current number is positive,
+                #all numbers after it is positve, can never add to 0
+                break
+
+            target = -(nums[i])
+            while l<r:
+                if nums[l]+nums[r] == target:
+                    res.append((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                elif nums[l]+nums[r] > target:
+                    r -= 1
+                elif nums[l]+nums[r] < target:
+                    l += 1
+
+        res = set(res)
+        for item in res:
+            item = list(item)
+        
+
+        return list(res)
